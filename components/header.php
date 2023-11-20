f008e2f5756eeb88c26a9ed30eb8aaadb4c60515
djmalibiran <33365949+djmalibiran@users.noreply.github.com>
2023-11-20 15:37:23 +0800

    Update header.php

--- components/header.php
+++ components/header.php
@@ -1,5 +1,6 @@
 <?php
 
+
 ?>
 <!doctype html>
 <html lang="en">
@@ -15,20 +16,17 @@
 <body>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container">
-            <a class="navbar-brand" href="#">Wilmer Student Exam</a>
+            <a class="navbar-brand" href="<?php echo "http://" . $_SERVER['HTTP_HOST'] ?>/wse/">Wilmer Student Exam</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link" href="<?php echo "http://" . $_SERVER['HTTP_HOST']?>/wse/">Home</a>
+                        <a class="nav-link" href="<?php echo "http://" . $_SERVER['HTTP_HOST'] ?>/wse/">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="<?php echo "http://" . $_SERVER['HTTP_HOST']?>/wse/test.php">Take the Test</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="<?php echo "http://" . $_SERVER['HTTP_HOST']?>/wse/results.php">Result</a>
+                        <a class="nav-link" href="<?php echo "http://" . $_SERVER['HTTP_HOST'] ?>/wse/results.php">Results</a>
                     </li>
                 </ul>
             </div>
